800f96e56bcd0de2b65864635c463f750ae72ee8
Storm <heike.storm@web.de>
2019-12-21 15:16:35 +0100

    dockerfile gui corrected

--- deployK8s.bat
+++ deployK8s.bat
@@ -8,5 +8,6 @@ docker push arwedstorm/auth-gui
 
 echo "Update Kubernetes culster .. "
 kubectl apply -f k8s
+kubectl apply -f k8s/local-k8s
 kubectl set image deployment auth-api-deployment auth-api=arwedstorm/auth-api:latest
 kubectl set image deployment auth-gui-deployment auth-gui=arwedstorm/auth-gui:latest
